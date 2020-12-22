2694337d68192099647f35bc1ba26bde948f7192
Jerajo <jessejose@outlook.es>
2020-12-22 17:34:42 -0400

    MainPage changed to LoginPage

---
+++ PVBot/PVBot.Clients.UI/Views/LoginView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace PVBotClients.UI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginView
+    {
+        public LoginView()
+        {
+        }
+    }
+}
