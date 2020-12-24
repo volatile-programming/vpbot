c524e8053e0c011e68cf1a00fea5d3791f572232
Jerajo <jessejose@outlook.es>
2020-12-24 17:48:05 -0400

    login viewmodel updated

--- PVBot/PVBot.Clients.UI/ViewModels/LoginViewModel.cs
+++ PVBot/PVBot.Clients.UI/ViewModels/LoginViewModel.cs
@@ -1,13 +1,23 @@
-﻿using Prism.Navigation;
+﻿using Prism.Commands;
+using Prism.Navigation;
+using PVBot.Application.Commands;
+using PVBot.DataObjects.Contracts;
 
 namespace PVBot.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
-        public LoginViewModel(INavigationService navigationService)
+        public DelegateCommand NavigateToChatView { get; set; }
+
+        public LoginViewModel(INavigationService navigationService, 
+            ICommandFactory commandFactory,
+            IQueryFactory queryFactory) 
             : base(navigationService)
         {
             Title = "Login Page";
+
+            NavigateToChatView = commandFactory.MakeCommand<LoginCommand, DelegateCommand>();
         }
+
     }
 }
