e9c4024de4062cad8d7c6fa09846a1a050fd7aa7
Jerajo <jessejose@outlook.es>
2020-12-24 17:45:06 -0400

    some refactory

---
+++ PVBot/PVBot.DataObjects/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PVBot.DataObjects.Models
+{
+    public class Message
+    {
+        public Message(string from, string to, string text, DateTime date)
+        {
+            From = from;
+            To = to;
+            Text = text;
+            Date = date;
+        }
+
+        public string From { get; }
+        public string To { get; }
+        public string Text { get; }
+        public DateTime Date { get; }
+    }
+}
---
+++ PVBot/PVBot.DataObjects/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PVBot.DataObjects.Models
+{
+    public class User
+    {
+        public User(string username, string image)
+        {
+            UserName = username;
+            Image = image;
+        }
+
+        public string UserName { get; }
+        public string Image { get; }
+    }
+}
