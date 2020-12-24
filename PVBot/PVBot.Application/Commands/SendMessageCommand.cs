03c93aa17ec4622f72d3355ed946a9ded53aa327
Jerajo <jessejose@outlook.es>
2020-12-24 17:42:22 -0400

    send message command implemented

---
+++ PVBot/PVBot.Application/Commands/SendMessageCommand.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.GuardClauses;
+using PVBot.DataObjects.Contracts;
+using PVBot.DataObjects.Models;
+
+namespace PVBot.Application.Commands
+{
+    public class SendMessageCommand : ICommand<Message>
+    {
+        private readonly ITwilioService _twilioService;
+
+        public SendMessageCommand(ITwilioService twilioService)
+        {
+            _twilioService = twilioService;
+        }
+
+        public void Execute(Message message)
+        {
+            Guard.Against.Null(message, nameof(message));
+
+            _twilioService.SendMessage(message);
+        }
+    }
+}
