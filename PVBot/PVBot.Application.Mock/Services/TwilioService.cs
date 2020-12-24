50560763196c642ae79c5f6d7a30047655bd8c7b
Jerajo <jessejose@outlook.es>
2020-12-24 17:46:28 -0400

    twilio service implementation added

---
+++ PVBot/PVBot.Application.Mock/Services/TwilioService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using PVBot.DataObjects.Contracts;
+using PVBot.DataObjects.Models;
+
+namespace PVBot.Application.Mock.Services
+{
+    public class TwilioService : ITwilioService
+    {
+        public List<Message> GetMessages() =>
+            new List<Message> {
+                new Message("Me", "Pvbot", "Hello", DateTime.Now),
+                new Message("Pvbot", "Me", "Hi!", DateTime.Now)
+            };
+
+        public Message ResiveMessage() =>
+                new Message("Pvbot", "Me", "Hi!", DateTime.Now);
+
+        public void SendMessage(Message message)
+        {
+            string format = "Message sended:\n From:{0}\nTo:{1}\nMessage:{2}\nDate:{3}";
+
+            Console.WriteLine(format,
+                message.From,
+                message.To,
+                message.Text,
+                message.Date);
+        }
+    }
+}
---
+++ PVBot/PVBot.Application/Services/TwilioService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using PVBot.DataObjects.Contracts;
+using PVBot.DataObjects.Models;
+
+namespace PVBot.Application.Services
+{
+    public class TwilioService : ITwilioService
+    {
+        public List<Message> GetMessages()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Message ResiveMessage()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SendMessage(Message message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
