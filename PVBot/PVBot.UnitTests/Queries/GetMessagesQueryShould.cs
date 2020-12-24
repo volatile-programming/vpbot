a4a384cdc7f22866b6674571883597174f12ac26
Jerajo <jessejose@outlook.es>
2020-12-24 17:47:32 -0400

    unit test added

---
+++ PVBot/PVBot.UnitTests/Queries/GetMessagesQueryShould.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PVBot.UnitTests.Queries
+{
+    [TestClass]
+    public class GetMessagesQueryShould
+    {
+    }
+}
---
+++ PVBot/PVBot.UnitTests/Services/TwillioServiceShould.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PVBot.Application.Mock.Services;
+using PVBot.DataObjects.Models;
+
+namespace PVBot.UnitTests.Services
+{
+    [TestClass]
+    public class TwillioServiceShould
+    {
+        private TwilioService _sut;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _sut = new TwilioService();
+        }
+
+        [TestMethod]
+        public void GetMessages()
+        {
+            var messages = _sut.GetMessages();
+
+            messages.Should().NotBeEmpty();
+            messages.Should().NotContainNulls();
+            messages.Should().AllBeOfType<Message>();
+        }
+
+        [TestMethod]
+        public void ResiveMessage()
+        {
+            var message = _sut.ResiveMessage();
+
+            message.Should().NotBeNull();
+            message.Should().BeAssignableTo<Message>();
+        }
+
+        [TestMethod]
+        public void SendMessage()
+        {
+            var message = _sut.ResiveMessage();
+
+            _sut.SendMessage(message);
+        }
+    }
+}
