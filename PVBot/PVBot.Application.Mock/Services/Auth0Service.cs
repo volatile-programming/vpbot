bea25373f8999cb0f679b9db554f0eb4c76f7670
Jerajo <jessejose@outlook.es>
2020-12-24 17:46:04 -0400

    auth0 implementetion added

---
+++ PVBot/PVBot.Application.Mock/Services/Auth0Service.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PVBot.DataObjects.Contracts;
+using PVBot.DataObjects.Models;
+
+namespace PVBot.Application.Services
+{
+    public class Auth0Service : IAuth0Service
+    {
+        public User Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
